16d5e61b7bd723571d6df2994356fd7ecebcbc61
summer68 <summer68@msu.edu>
2018-12-11 13:36:58 -0500

    Setup project starting point

---
+++ portfolio-2019/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <LandingBanner/>
+  </div>
+</template>
+
+<script>
+import LandingBanner from './components/LandingBanner.vue'
+import Normalize from './assets/styles/normalize.css'
+
+export default {
+  name: 'app',
+  components: {
+    LandingBanner
+  }
+}
+</script>
+
+<style>
+body, html, #app {
+  width: 100%; height: 100%;
+}
+</style>
---
+++ portfolio-2019/src/components/LandingBanner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="banner">
+    <h1 style="text-align:center; font-size:60px; margin: 0; padding: 0; color: #fff;">Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingBanner',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.banner {
+  height: 100%; width: 100%;
+
+  background: #8360c3;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(315deg, #2ebf91, #8360c3);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(315deg, #2ebf91, #8360c3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
