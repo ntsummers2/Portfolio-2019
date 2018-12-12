8bfbc68c09df9a43327602101446816ddcd486b1
Nicholas Summers <ntsummers2@gmail.com>
2018-12-12 14:39:55 -0500

    Change banner background image and change text color to light blue green

--- portfolio-2019/src/components/LandingBanner.vue
+++ portfolio-2019/src/components/LandingBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :style="{'background-image': 'url(' + require('../assets/images/bannerbackground.jpg') + ')'}" class="banner">
+  <div :style="{'background-image': 'url(' + require('../assets/images/bannerbackground2.jpg') + ')'}" class="banner">
     <div class="banner-overlay">
       <div class="banner-text-container">
         <div class="banner-text">
@@ -27,15 +27,15 @@ export default {
 .banner {
   height: 100%; width: 100%;
   background-repeat:no-repeat;
-  background-size:contain;
+  background-size:cover;
   background-position:center;
 
   &-overlay {
     height: 100%; width: 100%;
 
-    background: #8360c3;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to bottom, transparent, #383C69);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to bottom, rgba(75,81,142, .5), 15%, #383C69); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: #A3F7B5;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, transparent, #A3F7B5);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, rgba(64,201,162, .9) 50%, transparent 50%); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   }
 
   &-text-container {
