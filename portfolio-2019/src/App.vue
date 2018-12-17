36436ef0b3145a735e29b993eb27e037086f1c5d
Nicholas Summers <ntsummers2@gmail.com>
2018-12-17 14:38:49 -0500

    Add font-awesome to project and add new social sites section

--- portfolio-2019/src/App.vue
+++ portfolio-2019/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <LandingBanner/>
     <Projects/>
+    <SocialSites/>
   </div>
 </template>
 
 <script>
 import LandingBanner from './components/LandingBanner.vue'
 import Projects from './components/Projects.vue'
+import SocialSites from './components/SocialSites.vue'
 
 export default {
   name: 'app',
   components: {
     LandingBanner,
-    Projects
+    Projects,
+    SocialSites
   }
 }
 </script>
--- portfolio-2019/src/main.js
+++ portfolio-2019/src/main.js
@@ -4,6 +4,15 @@ import App from './App.vue'
 // Import Styles
 import './assets/styles/normalize.css'
 
+// Import Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faLinkedin, faCodepen } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGithub, faLinkedin, faCodepen)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
